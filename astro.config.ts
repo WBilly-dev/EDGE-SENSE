4f6874b8d19c35418d1a71321316a9639d0cdaa5
Will <wbilly.dev@gmail.com>
2024-03-30 04:33:33 -0400

    First Deploy In Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
+    ? 'https://edge-sense.vercel.app'
     : 'http://localhost:4321',
   integrations: [
     react(),
