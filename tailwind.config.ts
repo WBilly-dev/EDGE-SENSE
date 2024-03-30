6332374f059a5301d96919a330da44e28c71d1b4
Will <wbilly.dev@gmail.com>
2024-03-30 04:11:58 -0400

    Adding NextUI & Shadcn UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
+const { nextui } = require("@nextui-org/react");
 
 const config = {
   darkMode: ['class'],
@@ -73,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate') , nextui()],
 } satisfies Config
 
 export default config
